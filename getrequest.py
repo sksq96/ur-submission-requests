b9e1f92f9fe1bcf798be43339b3ed874a72f9edb
Shubham Chandel <chanshub@amazon.com>
2018-02-24 00:36:41 +0530

    Updated requirements

--- getrequest.py
+++ getrequest.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: chanshub
 # @Date:   2018-01-29 23:02:11
-# @Last Modified by:   chanshub
-# @Last Modified time: 2018-02-13 23:18:38
+# @Last Modified by:   Shubham Chandel
+# @Last Modified time: 2018-02-24 00:35:14
 
 
 import requests
@@ -17,11 +17,11 @@ def getHeader(token): return {'Authorization': token, 'Content-Length': '0'}
 def submission_request(headers):
 	try:
 		time_fields = ["created_at", "updated_at", "closed_at"]
-		for request in sorted([r for r in requests.get(SUBMISSION_REQUEST, headers=headers).json()], key=lambda r: r["id"]):
+		for request in sorted([r for r in requests.get(SUBMISSION_REQUEST, headers=headers).json()], key=lambda r: r["id"], reverse=True):
 			print(request["id"])
 			for time_field in time_fields:
-				when = arrow.get(request[time_field]).to('Asia/Kolkata').humanize()
-				print(f"\t{when}")
+				when = arrow.get(request[time_field]).to('local')
+				print("\t{}   {}".format(when.format("YYYY-MM-DD HH:mm"), when.humanize()))
 	except Exception as e:
 		print("Exception from submission_request()", str(e))
 
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 		"Poll the Udacity reviews API to get details of maximum 5 submission requests."
 	)
 	cmd_parser.add_argument('--auth-token', '-T', dest='token',
-		metavar='TOKEN', type=str,
+		metavar='TOKEN', type=str, required=True,
 		help="""Your Udacity auth token."""
 	)
 	args = cmd_parser.parse_args()
